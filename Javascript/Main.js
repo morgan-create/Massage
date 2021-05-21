7636944146e92fa6d307b4024542f6b0d50d3998
morgan-create <77931700+morgan-create@users.noreply.github.com>
2021-05-21 16:59:36 +0100

    Adding tabletop.js
    Linked to spreadsheet, still to link data to inner html

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -1,3 +1,14 @@
-$(".Main-image").click(function(){
-  $(".Main-image").hide();
-});
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/10iUKuEVJD8HGK6f3nXTBUUhcsSpd93-Rs6K3hRrOEQo/edit';
+
+
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+  function showInfo(data, tabletop){
+alert('woo you did it!');
+console.log(data);
+};
+  window.addEventListener('DOMContentLoaded', init)
