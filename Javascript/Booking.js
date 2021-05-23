617f5e97d1393396dc8b956a7ff547e94570d322
morgan-create <morganshanks@MorgBook-Air.broadband>
2021-05-23 13:03:06 +0100

    innerHTML
    made the containers for the dates and time slots and linked them to the spreadsheet. Only for 1st object, still figuring out how to create new container for each object. I'll get there eventually!

--- Javascript/Booking.js
+++ Javascript/Booking.js
@@ -1,15 +1,35 @@
 var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/10iUKuEVJD8HGK6f3nXTBUUhcsSpd93-Rs6K3hRrOEQo/edit';
 
 
+
   function init() {
     Tabletop.init( { key: publicSpreadsheetUrl,
                      callback: showInfo,
                      simpleSheet: true } )
-  }
+    }
 
-  function showInfo(data, tabletop){
-alert('woo you did it!');
-console.log(data);
-};
+function showInfo(data, tabletop){
+    console.log(data);
+    console.log(data[0])
+    console.log(data[1])
+    console.log(data[2])
+    console.log(data[3])
 
-  window.addEventListener('DOMContentLoaded', init)
+    document.querySelector("h2.day").innerHTML = data[0].day
+    document.querySelector("td.Slot1").innerHTML = data[0].Slot1
+    document.querySelector("td.Slot2").innerHTML = data[0].Slot2
+    document.querySelector("td.Slot3").innerHTML = data[0].Slot3
+     //THIS WORKS WOOOO
+
+    data.forEach(function(data){ 
+        
+    console.log(data.day);
+    console.log(data.Slot1);
+    console.log(data.Slot2);
+    console.log(data.Slot3);
+
+    }
+    )}; 
+
+
+  window.addEventListener('DOMContentLoaded', init);
