7345c87b5a4ba4d5fbce49ab3adc7da9e41c78c6
morgan-create <morganshanks@MorgBook-Air.broadband>
2021-05-23 10:09:07 +0100

    Fix
    Made changes on github while away without laptop, just syncing up with the laptop. already explained the changes

--- Javascript/Booking.js
+++ Javascript/Booking.js
@@ -1,4 +1,5 @@
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0rq0Aqup7ue_aQNY2oKrkDy1dVnkQ4mh8liS5td1p5fE7ItRpVbq-p0ZnwhFXxQbGneh--B42-lQk/pubhtml?gid=0&single=true';
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/10iUKuEVJD8HGK6f3nXTBUUhcsSpd93-Rs6K3hRrOEQo/edit';
+
 
   function init() {
     Tabletop.init( { key: publicSpreadsheetUrl,
@@ -6,46 +7,8 @@ var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ0r
                      simpleSheet: true } )
   }
 
-  function showInfo(data, tabletop) {
-    alert('Successfully processed!')
-    console.log(data);
-
-    data.forEach(function(data){
-      console.log(data.Person);
-      console.log(data.Slices);
-    })
-    
-  }
-
+  function showInfo(data, tabletop){
+alert('woo you did it!');
+console.log(data);
+};
   window.addEventListener('DOMContentLoaded', init)
-
-////////////////////////////////////////////
-
-  google.charts.load("current", {packages:["corechart"]});
-
-
-      google.charts.setOnLoadCallback(drawChart);
-
-
-      function drawChart() {
-        var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1Qn9Rrax5hSLGVEsXk7mt9ChYPrJhHk6W8ACHcSAUVU0/edit?usp=sharing');
-        query.send(handleQueryResponse);
-        }
-
-      function handleQueryResponse(response) {
-        var data = response.getDataTable();
-        var chart = new google.visualization.ColumnChart(document.getElementById('columnchart'));
-        chart.draw(data, null);
-
-
-
-        var options = {
-          title: 'My Daily Activities',
-          pieHole: 0.4,
-        };
-
-
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-        chart.draw(data, options);
-      }
