c4028db34edd7071a154d58f96c2b47c16a2f65b
morgan-create <morganshanks@MorgBook-Air.broadband>
2021-06-05 11:45:04 +0100

    Connected!
    Man i dont know why it took me so long when ive actually done the same thing before on a different project buuuut the booking page now creates the availability card with all the correct info from the spreadsheet. Finally. Granted theres probably a better solution than pasting the html needed into the innerhtml, but this works and its easy for me to change/understand

--- Javascript/Booking.js
+++ Javascript/Booking.js
@@ -11,18 +11,37 @@ var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/10iUKuEVJD8HG
 function showInfo(data, tabletop){
     console.log(data);
 
-    document.querySelector("h2.day").innerHTML = data[0].day
-    document.querySelector("td.Slot1").innerHTML = data[0].Slot1
-    document.querySelector("td.Slot2").innerHTML = data[0].Slot2
-    document.querySelector("td.Slot3").innerHTML = data[0].Slot3
+    var container = document.querySelector("div.cardContainer");
+    container.innerHTML= ""; 
+
+
+    //document.querySelector("h2.day").innerHTML = data[0].day
+    //document.querySelector("td.Slot1").innerHTML = data[0].Slot1
+    //document.querySelector("td.Slot2").innerHTML = data[0].Slot2
+    //document.querySelector("td.Slot3").innerHTML = data[0].Slot3
      //THIS WORKS WOOOO
-
+console.log(data.length)
+    
+  
     data.forEach(function(data){ 
-        
-    console.log(data)
+     
+    var day = data.day;
+    var slot1 = data.Slot1;
+    var slot2 = data.Slot2;
+    var slot3 = data.Slot3;
 
+    container.innerHTML += `<div class="center availabilityCard">
+    <h2 class="day">${day}</h2>
+    <table class="slots">
+      <tr>
+        <td class="Slot1">${slot1}</td>
+        <td class="Slot2">${slot2}</td>
+        <td class="Slot3">${slot3}</td>
+      </tr>
+    </table>
+  </div>`
     }
     )}; 
 
-
+  
   window.addEventListener('DOMContentLoaded', init);
